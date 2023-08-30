4ea106a03331cb3c3dab93b975da1ae3b8eba993
zhangxiaosa <hustzmx@gmail.com>
2023-08-30 03:38:08 +0000

    add build.sh

---
+++ build.sh
@@ -0,0 +1,6 @@
+#! /bin/bash
+
+cd /tmp/demystifying_probdd/src/picire
+SETUPTOOLS_SCM_PRETEND_VERSION=21.8 pip install .
+cd /tmp/demystifying_probdd/src/picireny
+SETUPTOOLS_SCM_PRETEND_VERSION=21.8 pip install .
