72e0a6afc809405c0f7fb1c7460b7be7a3ae32ce
zhangxiaosa <hustzmx@gmail.com>
2023-09-09 02:10:13 -0400

    debug

--- src/picire/picire/light_dd.py
+++ src/picire/picire/light_dd.py
@@ -21,8 +21,7 @@ class LightDD(AbstractDD):
     """
 
     def __init__(self, test, cache=None, id_prefix=(), onepass=False, start_from_n=None,
-                 split=config_splitters.zeller, subset_first=True, 
-                 subset_iterator=config_iterators.forward, complement_iterator=config_iterators.forward, **kwargs):
+                 **kwargs):
         """
         Initialize a LightDD object.
 
