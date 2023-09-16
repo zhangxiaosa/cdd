eb40d51db53d0899067bfb70557c7e164af87b01
zhangxiaosa <hustzmx@gmail.com>
2023-09-15 20:03:40 -0400

    add run chisel script

--- scripts/build_chisel.sh
+++ scripts/build_chisel.sh
@@ -1,7 +1,9 @@
 #! /bin/bash
-root=$(pwd)
+root=/home/coq/demystifying_probdd
 cd ${root}/src/chisel
 cmake -S . -B ${root}/build
 cd ${root}/build
+make
+cd ${root}/benchmarks/debloating
 make
 cd ${root}
