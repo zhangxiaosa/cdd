43ce5fc7726a15c516cae4d668c9d62efaaaf7f6
zhangxiaosa <hustzmx@gmail.com>
2023-09-17 16:37:46 -0400

    debug

--- scripts/summarize_chisel.py
+++ scripts/summarize_chisel.py
@@ -3,9 +3,9 @@ import re
 import sys
 import csv
 
-BENCHMARK_LIST = ['bzip2-1.0.5' 'chown-8.2' 'date-8.21' 'grep-2.19'
-                  'gzip-1.2.4' 'mkdir-5.2.1' 'rm-8.4' 'sort-8.16'
-                  'tar-1.14' 'uniq-8.16']
+BENCHMARK_LIST = ['bzip2-1.0.5', 'chown-8.2', 'date-8.21', 'grep-2.19',
+                  'gzip-1.2.4', 'mkdir-5.2.1', 'rm-8.4', 'sort-8.16',
+                  'tar-1.14', 'uniq-8.16']
 RESULT_PATH = sys.argv[1]
 
 def get_time_from_log(log_file):
