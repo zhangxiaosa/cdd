d45596abcda829ede79d70efd9c034552305fa63
zhangxiaosa <hustzmx@gmail.com>
2023-12-30 22:27:50 -0500

    add threshold to print

--- src/picire/picire/utils.py
+++ src/picire/picire/utils.py
@@ -1,10 +1,16 @@
-def generate_log(config, prefix="", print_idx=False):
+def generate_log(config, prefix="", print_idx=False, threshold=None):
     indices = []
     for item in config:
         indices.append(item)
     indices.sort()
     info_to_print = "\t%s " % prefix
-    info_to_print = info_to_print + "%d elements. " % len(indices)
+    info_to_print += "%d elements. " % len(indices)
+
     if print_idx:
-        info_to_print = info_to_print + "Idx: %r" % indices
+        if threshold != None:
+            if len(indices) > threshold:
+                info_to_print += "Idx: %r ... %r" % (indices[:5], indices[-5:])
+            else:
+                info_to_print += "Idx: %r" % indices
+
     return info_to_print
