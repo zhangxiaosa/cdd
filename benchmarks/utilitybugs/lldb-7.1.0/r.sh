7a0048c17f55aaf5f5814733eae4911828fbc131
codesubmissionfse24cdd <codesubmissionzzz@gmail.com>
2024-01-04 01:43:50 +0000

    find a larger input for lldb

--- benchmarks/utilitybugs/lldb-7.1.0/r.sh
+++ benchmarks/utilitybugs/lldb-7.1.0/r.sh
@@ -4,20 +4,28 @@ UTILITY="lldb"
 VERSION="7.1.0"
 BIN_PATH="${UTILITY}"
 
-TIMEOUT=30
+TIMEOUT=120
 
 timeout -s 9 $TIMEOUT valgrind $BIN_PATH < input > out.txt 2>&1
+ret=$?
+
+if [ $ret != 139 ]; then
+    exit 1
+fi
 
 # An array containing all the strings to check, each pattern on a new line
 check_strings=(
     "Process terminating with default action of signal 11 (SIGSEGV): dumping core"
-    "fatal_error (dl-error-skeleton.c:78)"
-    "_dl_signal_exception (dl-error-skeleton.c:102)"
-    "_dl_map_object_deps (dl-deps.c:612)"
-    "dl_main (rtld.c:1733)"
-    "_dl_sysdep_start (dl-sysdep.c:253)"
-    "_dl_start_final (rtld.c:415)"
-    "_dl_start (rtld.c:522)"
+    "lldb_private::Target::EvaluateExpression(llvm::StringRef, lldb_private::ExecutionContextScope*, lldb_private::SharingPtr<lldb_private::ValueObject>&, lldb_private::EvaluateExpressionOptions const&, std::__cxx11::basic_string<char, std::char_traits<char>, std::allocator<char> >*, lldb_private::ValueObject*) (Target.cpp:2399)"
+    "lldb_private::CommandInterpreter::PreprocessCommand(std::__cxx11::basic_string<char, std::char_traits<char>, std::allocator<char> >&) (CommandInterpreter.cpp:1529)"
+    "lldb_private::CommandInterpreter::HandleCommand(char const*, lldb_private::LazyBool, lldb_private::CommandReturnObject&, lldb_private::ExecutionContext*, bool, bool) (CommandInterpreter.cpp:1708)"
+    "lldb_private::CommandInterpreter::IOHandlerInputComplete(lldb_private::IOHandler&, std::__cxx11::basic_string<char, std::char_traits<char>, std::allocator<char> >&) (CommandInterpreter.cpp:2828)"
+    "lldb_private::IOHandlerEditline::Run() (IOHandler.cpp:577)"
+    "lldb_private::Debugger::ExecuteIOHandlers() (Debugger.cpp:995)"
+    "lldb_private::CommandInterpreter::RunCommandInterpreter(bool, bool, lldb_private::CommandInterpreterRunOptions&) (CommandInterpreter.cpp:3039)"
+    "lldb::SBDebugger::RunCommandInterpreter(bool, bool) (SBDebugger.cpp:1103)"
+    "Driver::MainLoop() (Driver.cpp:682)"
+    "main (Driver.cpp:889)"
     # Add more patterns here if needed
 )
 
