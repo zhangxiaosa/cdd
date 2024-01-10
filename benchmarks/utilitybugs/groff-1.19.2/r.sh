4091fec5a0fc738152aded1b6bdaa56305a3f35b
codesubmissionfse24cdd <codesubmissionzzz@gmail.com>
2024-01-10 20:48:18 +0000

    rebuild groff, and fix r.sh of groff

--- benchmarks/utilitybugs/groff-1.19.2/r.sh
+++ benchmarks/utilitybugs/groff-1.19.2/r.sh
@@ -1,11 +1,11 @@
 #! /bin/bash
 UTILITY="groff"
 VERSION="1.19.2"
-BIN_PATH="/home/coq/cdd/benchmarks/utilitybugs/${UTILITY}-${VERSION}/bin/${UTILITY}"
+BIN_PATH="${UTILITY}"
 
 TIMEOUT=30
 
-timeout -s 9 $TIMEOUT $BIN_PATH -mtty-char -Tascii < input > out.txt 2>&1
+timeout -s 9 $TIMEOUT $BIN_PATH < input > out.txt 2>&1
 ret=$?
 if [ $ret != 137 ]; then
 	exit 1
