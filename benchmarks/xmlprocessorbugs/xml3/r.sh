d9b19d48361dbe001816aee60d4c80a1bde2e44f
zhangxiaosa <hustzmx@gmail.com>
2024-02-03 21:12:31 +0000

    use abs path

--- benchmarks/xmlprocessorbugs/xml3/r.sh
+++ benchmarks/xmlprocessorbugs/xml3/r.sh
@@ -4,7 +4,7 @@ echo 'declare namespace r ="oOPqSKGThUaiMWmph";declare namespace oJn ="oOPqSKGTh
 
 # run saxon
 target="saxon"
-java -cp '../saxon-he-12.4.jar:../xmlresolver-5.2.0/lib/*' net.sf.saxon.Query -s:./input.xml -q:./query.xq > ${target}_raw_result.xml 2>&1
+java -cp '/data/m492zhan/cdd/cdd/benchmarks/xmlprocessorbugs/saxon-he-12.4.jar:/data/m492zhan/cdd/cdd/benchmarks/xmlprocessorbugs/xmlresolver-5.2.0/lib/*' net.sf.saxon.Query -s:./input.xml -q:./query.xq > ${target}_raw_result.xml 2>&1
 ret=$?
 
 if [ $ret != 0 ]; then
@@ -16,7 +16,7 @@ grep -o 'id="[^"]*"' ${target}_raw_result.xml | sed 's/id="//g' | sed 's/"//g' >
 
 # run basex
 target="basex"
-java -cp /data/m492zhan/review/icse24/artifact_evaluation/basex/basex-core/target/basex-10.5-SNAPSHOT.jar org.basex.BaseX -i input.xml query.xq > ${target}_raw_result.xml 2>&1
+java -cp "/data/m492zhan/review/icse24/artifact_evaluation/basex/basex-core/target/basex-10.5-SNAPSHOT.jar" org.basex.BaseX -i input.xml query.xq > ${target}_raw_result.xml 2>&1
 ret=$?
 
 if [ $ret != 0 ]; then
