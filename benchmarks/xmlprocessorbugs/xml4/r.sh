f0161841ec4804184770d9fea0009d42089e63af
zhangxiaosa <hustzmx@gmail.com>
2024-02-04 02:24:40 +0000

    update r.sh of xml4

--- benchmarks/xmlprocessorbugs/xml4/r.sh
+++ benchmarks/xmlprocessorbugs/xml4/r.sh
@@ -1,8 +1,10 @@
 #! /bin/bash
 
+echo 'declare namespace m ="QyrG";declare namespace DnpiK ="QyrG";declare namespace W ="ZlD";declare namespace zt ="QyrG";//*[boolean(sum(count(head(. ! (avg(position()),.)))) ! (count(.) + 1262593956)) and boolean(.//*/following::*)]//*' > query.xq
+
 # run saxon
 target="saxon"
-java -cp '../saxon-he-12.4.jar:../xmlresolver-5.2.0/lib/*' net.sf.saxon.Query -s:./input.xml -q:./query.xq > ${target}_raw_result.xml 2>&1
+java -cp '/data/m492zhan/cdd/cdd/benchmarks/xmlprocessorbugs/saxon-he-12.4.jar:/data/m492zhan/cdd/cdd/benchmarks/xmlprocessorbugs/xmlresolver-5.2.0/lib/*' net.sf.saxon.Query -s:./input.xml -q:./query.xq > ${target}_raw_result.xml 2>&1
 ret=$?
 
 if [ $ret != 0 ]; then
@@ -10,11 +12,11 @@ if [ $ret != 0 ]; then
 fi
 
 # process saxon result
-grep -o 'id="[^"]*"' ${target}_raw_result.xml | sed 's/id="//g' | sed 's/"//g' > ${target}_processed_result.txt
+grep -o 'id="[^"]*"' ${target}_raw_result.xml | sed 's/id="//g' | sed 's/"//g' | grep -v '^[[:space:]]*$' > ${target}_processed_result.txt
 
 # run basex
 target="basex"
-java -cp /data/m492zhan/review/icse24/artifact_evaluation/basex/basex-core/target/basex-10.5-SNAPSHOT.jar org.basex.BaseX -i input.xml query.xq > ${target}_raw_result.xml 2>&1
+java -cp "/data/m492zhan/review/icse24/artifact_evaluation/basex/basex-core/target/basex-10.5-SNAPSHOT.jar" org.basex.BaseX -i input.xml query.xq > ${target}_raw_result.xml 2>&1
 ret=$?
 
 if [ $ret != 0 ]; then
@@ -22,7 +24,7 @@ if [ $ret != 0 ]; then
 fi
 
 # process basex result
-grep -o 'id="[^"]*"' ${target}_raw_result.xml | sed 's/id="//g' | sed 's/"//g' > ${target}_processed_result.txt
+grep -o 'id="[^"]*"' ${target}_raw_result.xml | sed 's/id="//g' | sed 's/"//g' | grep -v '^[[:space:]]*$' > ${target}_processed_result.txt
 
 # diff
 if diff saxon_processed_result.txt basex_processed_result.txt > /dev/null 2>&1; then
