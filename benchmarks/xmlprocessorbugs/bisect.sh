ebabedad1f3243022a697df96e0bad1ffa5756b0
zhangxiaosa <hustzmx@gmail.com>
2024-02-17 19:07:04 +0000

    update bisect.sh

--- benchmarks/xmlprocessorbugs/bisect.sh
+++ benchmarks/xmlprocessorbugs/bisect.sh
@@ -1,18 +1,19 @@
 #!/bin/bash
 
 # 进入git仓库目录
-repo="/data/m492zhan/review/icse24/artifact_evaluation/basex"
+repo="/data/m492zhan/review/icse24/artifact_evaluation/basex_bisect"
 cd $repo
-
+latest_version="3c26407"
+oldest_version="d1bb20b"
 git reset --hard
 
-git checkout 3c26407
+git checkout $latest_version
 /data/m492zhan/cdd/cdd/benchmarks/xmlprocessorbugs/bisect_script.sh
 if [ $? -eq 0 ]; then
     echo "bad init commit not bad"
     exit 1
 fi
-git checkout d1bb20b
+git checkout $oldest_version
 /data/m492zhan/cdd/cdd/benchmarks/xmlprocessorbugs/bisect_script.sh
 if [ $? -eq 1 ]; then
     echo "good init commit not good"
@@ -22,8 +23,8 @@ fi
 
 # 初始化git bisect
 git bisect start
-git bisect bad 3c26407
-git bisect good d1bb20b
+git bisect bad $latest_version
+git bisect good $oldest_version
 
 # 定义bisect运行时的脚本
 git bisect run /data/m492zhan/cdd/cdd/benchmarks/xmlprocessorbugs/bisect_script.sh
