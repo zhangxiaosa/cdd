cd19fbeec82a32297b565228d81d1b4e0cf29877
zhangxiaosa <hustzmx@gmail.com>
2024-02-22 15:21:06 +0000

    add xml58

---
+++ benchmarks/xmlprocessorbugs/xml58/r.sh
@@ -0,0 +1,38 @@
+#! /bin/bash
+
+echo 'declare namespace ZF ="TqSgHhbHSwH";declare namespace jdYX ="CkRmpYRETdaWjNRGg";declare namespace qo ="TqSgHhbHSwH";declare namespace Av ="CkRmpYRETdaWjNRGg";//*[((not(. = <A>2</A>) and not(not((sum(@f2) > 78390.3750) != true()))) and boolean(@id >= -1879133140)) and not(boolean(avg(reverse(reverse(subsequence(count(@id), 82))))))]' > query.xq
+
+# run saxon
+target="saxon"
+java -cp '/data/m492zhan/cdd/cdd/benchmarks/xmlprocessorbugs/saxon-he-12.4.jar:/data/m492zhan/cdd/cdd/benchmarks/xmlprocessorbugs/xmlresolver-5.2.0/lib/*' net.sf.saxon.Query -s:./input.xml -q:./query.xq > ${target}_raw_result.xml 2>&1
+ret=$?
+
+if [ $ret != 0 ]; then
+  exit 1
+fi
+
+# process saxon result
+grep -o 'id="[^"]*"' ${target}_raw_result.xml | sed 's/id="//g' | sed 's/"//g' | grep -v '^[[:space:]]*$' > ${target}_processed_result.txt
+
+# run basex
+target="basex"
+#java -cp "/data/m492zhan/review/icse24/artifact_evaluation/basex-bisect/basex-core/target/basex.jar" org.basex.BaseX -i input.xml query.xq > ${target}_raw_result.xml 2>&1
+java -cp "/data/m492zhan/review/icse24/artifact_evaluation/basex-7165a07.jar" org.basex.BaseX -i input.xml query.xq > ${target}_raw_result.xml 2>&1
+ret=$?
+
+if [ $ret != 0 ]; then
+  exit 1
+fi
+
+# process basex result
+grep -o 'id="[^"]*"' ${target}_raw_result.xml | sed 's/id="//g' | sed 's/"//g' | grep -v '^[[:space:]]*$' > ${target}_processed_result.txt
+
+# diff
+if diff saxon_processed_result.txt basex_processed_result.txt > /dev/null 2>&1; then
+    # files are different
+    exit 1
+fi
+
+exit 0
+
+
