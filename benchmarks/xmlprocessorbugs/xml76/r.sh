4beece299997c30b3c750576c1b2279b945087d4
zhangxiaosa <hustzmx@gmail.com>
2024-02-23 19:25:39 +0000

    add xml76

---
+++ benchmarks/xmlprocessorbugs/xml76/r.sh
@@ -0,0 +1,39 @@
+#! /bin/bash
+
+echo 'declare namespace TN ="ksvqwMV";declare namespace Vwc ="AAxhnTXMdFzQQANR";declare namespace MOkN ="yHB";declare namespace DLaY ="xtSMwMMa";declare namespace tGEcc ="ksvqwMV";declare namespace RJ ="ZOAbtTpwqyFGl";declare namespace zPXeZ ="yHB";declare namespace cI ="CurVaPjqOEA";declare namespace ontPh ="tSEEtRPBsvIWUkW";declare namespace Vks ="AAxhnTXMdFzQQANR";declare namespace Zf ="Ugdj";//*[not(has-children() = false())]' > query.xq
+
+# run saxon
+target="saxon"
+java -cp '/data/m492zhan/cdd/cdd/benchmarks/xmlprocessorbugs/saxon-he-12.4.jar:/data/m492zhan/cdd/cdd/benchmarks/xmlprocessorbugs/xmlresolver-5.2.0/lib/*' net.sf.saxon.Query -s:./input.xml -q:./query.xq > ${target}_raw_result.xml 2>&1
+ret=$?
+
+if [ $ret != 0 ]; then
+  exit 1
+fi
+
+# process saxon result
+grep -o 'id="[^"]*"' ${target}_raw_result.xml | sed 's/id="//g' | sed 's/"//g' | grep -v '^[[:space:]]*$' > ${target}_processed_result.txt
+
+# run basex
+target="basex"
+#java -cp "/data/m492zhan/review/icse24/artifact_evaluation/basex-bisect/basex.jar" org.basex.BaseX -i input.xml query.xq > ${target}_raw_result.xml 2>&1
+#java -cp "/tmp/basex.jar" org.basex.BaseX -i input.xml query.xq > ${target}_raw_result.xml 2>&1
+java -cp "/data/m492zhan/review/icse24/artifact_evaluation/basex-5b34391.jar" org.basex.BaseX -i input.xml query.xq > ${target}_raw_result.xml 2>&1
+ret=$?
+
+if [ $ret != 0 ]; then
+  exit 1
+fi
+
+# process basex result
+grep -o 'id="[^"]*"' ${target}_raw_result.xml | sed 's/id="//g' | sed 's/"//g' | grep -v '^[[:space:]]*$' > ${target}_processed_result.txt
+
+# diff
+if diff saxon_processed_result.txt basex_processed_result.txt > /dev/null 2>&1; then
+    # files are different
+    exit 1
+fi
+
+exit 0
+
+
