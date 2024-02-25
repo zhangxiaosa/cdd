c6230f71627eb9d4e8d8be070c88d60fc7d8cd0e
zhangxiaosa <hustzmx@gmail.com>
2024-02-24 20:41:47 -0500

    xml43

--- benchmarks/xmlprocessorbugs/xml43/r.sh
+++ benchmarks/xmlprocessorbugs/xml43/r.sh
@@ -1,10 +1,32 @@
 #! /bin/bash
 
-echo 'declare namespace Aa ="saEqczELwiDr";declare namespace eTA ="NrJTEVj";declare namespace A ="xBgwTrJDHDdbRMXBx";declare namespace ycMC ="iFMLJyyqIYnZTtwzRjQf";declare namespace feMuH ="WyfHLLUDdrTuJjNxgxH";declare namespace VrN ="X";declare namespace CrEt ="iF";declare namespace EU ="fLxguhNPLNhmUoaCLj";declare namespace yAcX ="QcMEgKoMhEpBY";declare namespace CIIz ="NrJTEVj";declare namespace s ="xBgwTrJDHDdbRMXBx";declare namespace ibfTi ="saEqczELwiDr";//*/ancestor::*[reverse(boolean(reverse(B29))) or not(boolean(normalize-space(@e9)))]/CIIz:G5/preceding::VrN:U5[not((boolean(tail(./(/U13/S11/E16/F5/T6/H7/U5))) and false()) ! head(.))]/parent::*[(boolean(last()) or ((head(head(head(head(subsequence(., 2))/node-name()))) ! .) castable as xs:duration)) or (reverse(boolean(.)) = false()) = false()]/Aa:T31[boolean((node-name() ! count(subsequence(., 90))) + -299134339) and (boolean(last()) and not(boolean(subsequence(tail(./preceding-sibling::Aa:E3), 2))))]/parent::VrN:H7[boolean(./*//*[not(boolean(O31))])]/*' > query.xq
+QUERY='declare namespace Aa ="saEqczELwiDr";declare namespace eTA ="NrJTEVj";declare namespace A ="xBgwTrJDHDdbRMXBx";declare namespace ycMC ="iFMLJyyqIYnZTtwzRjQf";declare namespace feMuH ="WyfHLLUDdrTuJjNxgxH";declare namespace VrN ="X";declare namespace CrEt ="iF";declare namespace EU ="fLxguhNPLNhmUoaCLj";declare namespace yAcX ="QcMEgKoMhEpBY";declare namespace CIIz ="NrJTEVj";declare namespace s ="xBgwTrJDHDdbRMXBx";declare namespace ibfTi ="saEqczELwiDr";//*/ancestor::*[reverse(boolean(reverse(B29))) or not(boolean(normalize-space(@e9)))]/CIIz:G5/preceding::VrN:U5[not((boolean(tail(./(/U13/S11/E16/F5/T6/H7/U5))) and false()) ! head(.))]/parent::*[(boolean(last()) or ((head(head(head(head(subsequence(., 2))/node-name()))) ! .) castable as xs:duration)) or (reverse(boolean(.)) = false()) = false()]/Aa:T31[boolean((node-name() ! count(subsequence(., 90))) + -299134339) and (boolean(last()) and not(boolean(subsequence(tail(./preceding-sibling::Aa:E3), 2))))]/parent::VrN:H7[boolean(./*//*[not(boolean(O31))])]/*'
+GOOD_VERSION="1e9bc83"
+BAD_VERSION="816b386"
+
+echo $QUERY > query.xq
 
 # run saxon
-target="saxon"
-java -cp '/data/m492zhan/cdd/cdd/benchmarks/xmlprocessorbugs/saxon-he-12.4.jar:/data/m492zhan/cdd/cdd/benchmarks/xmlprocessorbugs/xmlresolver-5.2.0/lib/*' net.sf.saxon.Query -s:./input.xml -q:./query.xq > ${target}_raw_result.xml 2>&1
+target_saxon="saxon"
+java -cp '/home/coq/cdd/benchmarks/xmlprocessorbugs/lib/saxon-he-12.4.jar:/home/coq/cdd/benchmarks/xmlprocessorbugs/lib/xmlresolver-5.2.0/lib/*' net.sf.saxon.Query -s:./input.xml -q:./query.xq > ${target_saxon}_raw_result.xml 2>&1
+ret=$?
+
+if [ $ret != 0 ]; then
+  exit 1
+fi
+
+# run basex_bad
+target_basex_bad="basex_bad"
+java -cp "/home/coq/cdd/benchmarks/xmlprocessorbugs/lib/basex-${BAD_VERSION}.jar" org.basex.BaseX -i input.xml query.xq > ${target_basex_bad}_raw_result.xml 2>&1
+ret=$?
+
+if [ $ret != 0 ]; then
+  exit 1
+fi
+
+# run basex_good
+target_basex_good="basex_good"
+java -cp "/home/coq/cdd/benchmarks/xmlprocessorbugs/lib/basex-${GOOD_VERSION}.jar" org.basex.BaseX -i input.xml query.xq > ${target_basex_good}_raw_result.xml 2>&1
 ret=$?
 
 if [ $ret != 0 ]; then
@@ -12,23 +34,22 @@ if [ $ret != 0 ]; then
 fi
 
 # process saxon result
-grep -o 'id="[^"]*"' ${target}_raw_result.xml | sed 's/id="//g' | sed 's/"//g' | grep -v '^[[:space:]]*$' > ${target}_processed_result.txt
+grep -o 'id="[^"]*"' ${target_saxon}_raw_result.xml | sed 's/id="//g' | sed 's/"//g' | grep -v '^[[:space:]]*$' > ${target_saxon}_processed_result.txt
 
-# run basex
-target="basex"
-java -cp "/data/m492zhan/review/icse24/artifact_evaluation/basex/basex-core/target/basex-10.5-SNAPSHOT.jar" org.basex.BaseX -i input.xml query.xq > ${target}_raw_result.xml 2>&1
-ret=$?
+# process basex_bad result
+grep -o 'id="[^"]*"' ${target_basex_bad}_raw_result.xml | sed 's/id="//g' | sed 's/"//g' | grep -v '^[[:space:]]*$' > ${target_basex_bad}_processed_result.txt
 
-if [ $ret != 0 ]; then
-  exit 1
+# process basex_good result
+grep -o 'id="[^"]*"' ${target_basex_good}_raw_result.xml | sed 's/id="//g' | sed 's/"//g' | grep -v '^[[:space:]]*$' > ${target_basex_good}_processed_result.txt
+
+
+# diff, files should be different
+if diff ${target_saxon}_processed_result.txt ${target_basex_bad}_processed_result.txt > /dev/null 2>&1; then
+    exit 1
 fi
 
-# process basex result
-grep -o 'id="[^"]*"' ${target}_raw_result.xml | sed 's/id="//g' | sed 's/"//g' | grep -v '^[[:space:]]*$' > ${target}_processed_result.txt
-
-# diff
-if diff saxon_processed_result.txt basex_processed_result.txt > /dev/null 2>&1; then
-    # files are different
+# diff, files should be same
+if ! diff ${target_saxon}_processed_result.txt ${target_basex_good}_processed_result.txt > /dev/null 2>&1; then
     exit 1
 fi
 
