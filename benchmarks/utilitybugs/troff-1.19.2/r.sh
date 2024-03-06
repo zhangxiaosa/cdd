f381d25ead4d45381142f90cd37adfc5209cb8b9
codesubmissionfse24cdd <codesubmissionzzz@gmail.com>
2024-03-06 17:47:03 +0000

    update troff script and binary

--- benchmarks/utilitybugs/troff-1.19.2/r.sh
+++ benchmarks/utilitybugs/troff-1.19.2/r.sh
@@ -2,7 +2,8 @@
 
 UTILITY="troff"
 VERSION="1.19.2"
-BIN_PATH="${UTILITY}"
+BIN_PATH="/home/coq/cdd/benchmarks/utilitybugs/${UTILITY}-${VERSION}/bin/${UTILITY}"
+export GROFF_FONT_PATH=/usr/local/share/groff/1.22.3/
 
 TIMEOUT=30
 
@@ -16,13 +17,12 @@ fi
 # An array containing all the strings to check, each pattern on a new line
 check_strings=(
     "Process terminating with default action of signal 11 (SIGSEGV): dumping core"
-    "font_info::get_tfont(font_size, int, int, int) (node.cpp:262)"
-    "special_node::special_node(macro const&, int) (node.cpp:3827)"
-    "do_special (input.cpp:5248)"
-    "token::next() (input.cpp:2142)"
-    "process_input_stack() (input.cpp:2778)"
-    "process_input_file(char const*) (input.cpp:7149)"
-    "main (input.cpp:7456)"
+    "font_info::get_tfont(font_size, int, int, int) (node.cpp:282)"
+    "special_node::special_node(macro const&, int) (node.cpp:3894)"
+    "token::next() (input.cpp:2226)"
+    "process_input_stack() (input.cpp:2866)"
+    "process_input_file(char const*) (input.cpp:7804)"
+    "main (input.cpp:8112)"
     # Add more patterns here if needed
 )
 
