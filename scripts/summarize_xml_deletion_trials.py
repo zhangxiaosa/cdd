07494c7e5a185eb0f4aa1f85f5b847e293dffa4a
zhangxiaosa <hustzmx@gmail.com>
2024-03-19 11:03:20 -0400

    xml summarize

--- scripts/summarize_xml_deletion_trials.py
+++ scripts/summarize_xml_deletion_trials.py
@@ -9,6 +9,7 @@ BENCHMARK_LIST = ['xml-071d221-1', 'xml-071d221-2', 'xml-1e9bc83-1', 'xml-1e9bc8
                   'xml-4c99b96-3', 'xml-4c99b96-4', 'xml-4c99b96-5', 'xml-4c99b96-6', 'xml-4c99b96-7',
                   'xml-4c99b96-8', 'xml-4c99b96-9', 'xml-8ede045-1', 'xml-8ede045-2', 'xml-8ede045-3',
                   'xml-8ede045-4', 'xml-8ede045-5', 'xml-8ede045-6', 'xml-8ede045-7', 'xml-8ede045-8', 'xml-f053486-1']
+S0 = 40
 
 def process_results_file(result_path):
     try:
@@ -21,11 +22,17 @@ def process_results_file(result_path):
         delete_size_fail_sum = 0
         num_success = 0
         num_fail = 0
+        num_complement = 0
+        num_success_complement = 0
+        num_repeated = 0
+        num_success_repeated = 0
+        num_large = 0
+        num_success_large = 0
         num_all = 0
         delete_size_frequency = {}  # To keep track of the frequencies
 
         for line in lines:
-            benchmark, total_size, delete_size, status = line.strip().split(', ')
+            benchmark, total_size, delete_size, complement, repeated, status = line.strip().split(', ')
             if benchmark not in BENCHMARK_LIST:
                 continue
             num_all += 1
@@ -48,6 +55,22 @@ def process_results_file(result_path):
                 num_fail += 1
                 delete_size_fail_sum += delete_size
 
+            if complement == "True":
+                num_complement += 1
+                if status == "success":
+                    num_success_complement += 1
+
+            if repeated == "True":
+                num_repeated += 1
+                if status == "success":
+                    num_success_repeated += 1
+
+            if complement == "False" and repeated == "False" and delete_size > S0:
+                num_large += 1
+                if status == "success":
+                    num_success_large += 1
+
+
         mean_list_size = total_size_sum / num_all if num_all else 0
         mean_delete_size_all = delete_size_sum / num_all if num_all else 0
         mean_delete_size_success = delete_size_success_sum / num_success if num_success else 0
@@ -61,6 +84,15 @@ def process_results_file(result_path):
             "mean_delete_size_all": mean_delete_size_all,
             "mean_delete_size_success": mean_delete_size_success,
             "mean_delete_size_fail": mean_delete_size_fail,
+            "query_complement_num_all": num_complement,
+            "query_complement_num_success": num_success_complement,
+            "query_complement_num_fail": num_complement - num_success_complement,
+            "query_repeated_num_all": num_repeated,
+            "query_repeated_num_success": num_success_repeated,
+            "query_repeated_num_fail": num_repeated - num_success_repeated,
+            "query_large_num_all": num_large,
+            "query_large_num_success": num_success_large,
+            "query_large_num_fail": num_large - num_success_large,
         }
 
         # Adding delete_size_frequency to the results
