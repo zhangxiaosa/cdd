4c1fe1702e140c94ee85207870e660d0fdc086f0
zhangxiaosa <hustzmx@gmail.com>
2024-07-20 23:00:12 -0400

    delete look, as cannot pass sanity check

--- scripts/cleanup_line_reduction.py
+++ scripts/cleanup_line_reduction.py
@@ -11,7 +11,7 @@ def delete_non_input_files(path):
 
 
 BENCHMARK_LIST = ['as-2.30', 'bison-3.0.4', 'checknr-8.1', 'ctags-8.4', 'dc-1.3', 'dc-1.4', 'flex-2.5.39',
-                  'gdb-8.1', 'indent-5.17', 'lldb-7.1.0', 'look-8.2', 'ptx-8.32', 'spell-1.1', 'troff-1.19.2']
+                  'gdb-8.1', 'indent-5.17', 'lldb-7.1.0', 'ptx-8.32', 'spell-1.1', 'troff-1.19.2']
 
 RESULT_PATH = sys.argv[1]
 
