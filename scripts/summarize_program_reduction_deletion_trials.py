665e1a0c4d51e6d7e9af13ade2951fa76303a42c
zhangxiaosa <hustzmx@gmail.com>
2024-12-12 00:24:49 -0500

    debug

--- scripts/summarize_program_reduction_deletion_trials.py
+++ scripts/summarize_program_reduction_deletion_trials.py
@@ -1,8 +1,12 @@
 import sys
 
-BENCHMARK_LIST = ['clang-22382', 'clang-23353',
-                  'clang-25900', 'clang-27747', 'gcc-59903', 'gcc-61383',
-                  'gcc-61917', 'gcc-65383', 'gcc-71626']
+BENCHMARK_LIST = [
+    'clang-22382', 'clang-22704', 'clang-23309', 'clang-23353',
+    'clang-25900', 'clang-26760', 'clang-27137', 'clang-27747',
+    'clang-31259', 'gcc-59903', 'gcc-60116', 'gcc-61383',
+    'gcc-61917', 'gcc-64990', 'gcc-65383', 'gcc-66186',
+    'gcc-66375', 'gcc-70127', 'gcc-70586', 'gcc-71626'
+]
 
 S0 = 10
 
